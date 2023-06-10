35e13eb21360d8cf8fdbd0745acc10f07ccf0bf0
Sebastian Jankowski <41026189+sjankowskim@users.noreply.github.com>
2023-06-10 17:17:55 -0400

    Add files via upload

---
+++ Unimbue.cs
@@ -0,0 +1,27 @@
+﻿using ThunderRoad;
+
+namespace Unimbue
+{
+    public class UnimbueSpell : SpellCastCharge
+    {
+    }
+
+    public class Unimbue : ThunderScript
+    {
+        public override void ScriptUpdate()
+        {
+            base.ScriptUpdate();
+
+            foreach (Item item in Item.all)
+            {
+                foreach (Imbue imbue in item.imbues)
+                {
+                    if (imbue.spellCastBase != null && imbue.spellCastBase.id.Equals("Unimbue"))
+                    {
+                        imbue.energy = 0;
+                    }
+                }
+            }
+        }
+    }
+}
